1f05ab0ac8036cc290d4ba1ab68f9eb1b1623a9d
Sahil Kullar <sahil_kullar@outlook.com>
2022-10-24 09:36:53 +0530

    console added

--- just-js/array-methods.js
+++ just-js/array-methods.js
@@ -27,3 +27,21 @@ Array.prototype.myReduce = function (callback, initialValue) {
   }
   return accumulator;
 };
+
+const array = [1, 2, 3];
+
+const mappedArray = array.myMap((value, i, arr) => {
+  return value + i + arr[1];
+});
+
+const filteredArray = array.myFilter((value, i, arr) => {
+  return value + i + arr[1] > 5;
+});
+
+const reducedValue = array.myReduce((accumulator, value, i, arr) => {
+  return accumulator + value + i + arr[1];
+}, 3);
+
+console.log(mappedArray);
+console.log(filteredArray);
+console.log(reducedValue);
