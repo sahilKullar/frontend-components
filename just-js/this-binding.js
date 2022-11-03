52bc85d04a540e8e6fead649338e6bc4679bb245
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-03 23:58:28 +0530

    console logs added

--- just-js/this-binding.js
+++ just-js/this-binding.js
@@ -13,3 +13,15 @@ Function.prototype.myApply = function (thisContext, args = []) {
 Function.prototype.myBind = function (thisContext, ...args) {
   return (...newArgs) => this.myApply(thisContext, [...args, ...newArgs]);
 };
+
+const obj = { num: 0 };
+
+function logNums(x, y) {
+  console.log(this.num, x, y);
+}
+
+logNums.myCall(obj, 1, 2);
+logNums.myApply(obj, [1, 2]);
+
+const boundFunction = logNums.myBind(obj, 1);
+boundFunction(2);
