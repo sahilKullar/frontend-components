1a437d2b482f18d753306c8aa056635df6996890
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-06 21:57:39 +0530

    fixed

--- decode-js/arrays/is-monotonic.js
+++ decode-js/arrays/is-monotonic.js
@@ -2,8 +2,8 @@ function isMonotonic(array) {
   let isNonDecreasing = true;
   let isNonIncreasing = true;
   for (let i = 1; i < array.length; i++) {
-    if (array < array[i - 1]) isNonDecreasing = false;
-    if (array > array[i - 1]) isNonIncreasing = false;
+    if (array[i] < array[i - 1]) isNonDecreasing = false;
+    if (array[i] > array[i - 1]) isNonIncreasing = false;
   }
   return isNonDecreasing || isNonIncreasing;
 }
