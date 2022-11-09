57d7767e0c5fb8987810f39cacc3a5e90d3fc017
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-09 23:47:29 +0530

    improved the program

--- decode-js/arrays/two-number-sum.js
+++ decode-js/arrays/two-number-sum.js
@@ -1,11 +1,29 @@
+// function twoNumberSum(array, targetSum) {
+//   const nums = {};
+//   for (const num of array) {
+//     const potentialMatch = targetSum - num;
+//     if (potentialMatch in nums) {
+//       return [potentialMatch, num];
+//     } else {
+//       nums[num] = true;
+//     }
+//   }
+//   return [];
+// }
+
 function twoNumberSum(array, targetSum) {
-  const nums = {};
-  for (const num of array) {
-    const potentialMatch = targetSum - num;
-    if (potentialMatch in nums) {
-      return [potentialMatch, num];
-    } else {
-      nums[num] = true;
+  array.sort((a, b) => a - b);
+  let leftIdx = 0;
+  let rightIdx = array.length - 1;
+
+  while (leftIdx <= rightIdx) {
+    const currentSum = array[leftIdx] + array[rightIdx];
+    if (currentSum === targetSum) {
+      return [array[leftIdx], array[rightIdx]];
+    } else if (currentSum < targetSum) {
+      leftIdx++;
+    } else if (currentSum > targetSum) {
+      rightIdx--;
     }
   }
   return [];
