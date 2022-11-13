496875d3ca7563e4ad53329db5e0325f8c8c37ed
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-13 19:42:27 +0530

    fixed testing framework implementation

--- just-js/testing-framework.js
+++ just-js/testing-framework.js
@@ -1,8 +1,24 @@
-function describe(testSuiteName, func) {}
+function describe(testSuiteName, func) {
+  console.log(`beginning test suite ${testSuiteName}`);
+  try {
+    func();
+    console.log(`successfully completed test suite ${testSuiteName}`);
+  } catch (error) {
+    const { testCaseName, errorMessage } = error;
+    console.error(
+      `failed running test suite ${testSuiteName} on test case ${testCaseName} with error message ${errorMessage}`
+    );
+  }
+}
 
 function it(testCaseName, func) {
+  console.log(`beginning test case ${testCaseName}`);
   try {
-  } catch (e) {}
+    func();
+    console.log(`successfully completed test case ${testCaseName}`);
+  } catch (errorMessage) {
+    throw { testCaseName, errorMessage };
+  }
 }
 
 function expect(actual) {
@@ -16,7 +32,6 @@ class ExpectFunctions {
   }
 
   toExist() {
-    // this.actual === null && this.actual === undefined
     if (this.actual == null) {
       throw `expected value to exist but got ${this.stringifiedActual}`;
     }
@@ -38,3 +53,31 @@ class ExpectFunctions {
     }
   }
 }
+
+describe("Passing Test Suite", () => {
+  it("Passing Test Case #1", function () {
+    expect("foo").toExist();
+    expect(1 + 1).toBe(2);
+  });
+
+  it("Passing Test Case #2", function () {
+    expect({}).toBeType("object");
+  });
+});
+
+console.log("---------------------------------------------------------------");
+
+describe("Failing Test Suite", () => {
+  it("Passing Test Case", function () {
+    expect(0).toBe(0);
+  });
+
+  it("Failing Test Case", function () {
+    expect(true).toBe(true);
+    expect(true).toBe(false);
+  });
+
+  it("Failing Test Case", function () {
+    expect("foo").toBe("bar");
+  });
+});
