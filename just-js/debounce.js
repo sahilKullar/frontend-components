5bbef9dc04f9584c9e3504b7db2b06e1c7ad4aca
Sahil Kullar <sahil_kullar@outlook.com>
2022-12-01 00:48:04 +0530

    added more logs for debounce func

--- just-js/debounce.js
+++ just-js/debounce.js
@@ -16,3 +16,17 @@ function debounce(callback, delay, immediate = false) {
     }, delay);
   };
 }
+
+const debounced = debounce(console.log, 3000, true);
+debounced("hello! 1");
+debounced("hello! 2");
+debounced("hello! 3");
+debounced("hello! 4");
+
+console.log("--------");
+
+const debouncedImmediately = debounce(console.log, 3000, false);
+debouncedImmediately("hello! 1");
+debouncedImmediately("hello! 2");
+debouncedImmediately("hello! 3");
+debouncedImmediately("hello! 4");
