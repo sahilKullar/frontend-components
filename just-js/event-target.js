278d1ab44864f0595320df26c563de5d4f35a71d
Sahil Kullar <sahil_kullar@outlook.com>
2023-03-09 15:54:24 +0530

    event target console added

--- just-js/event-target.js
+++ just-js/event-target.js
@@ -19,3 +19,12 @@ class EventTarget {
     this.listeners[name]?.forEach((callback) => callback());
   }
 }
+
+const target = new EventTarget();
+const logHello = () => console.log("hello");
+const logWorld = () => console.log("world");
+
+target.addEventListener("hello", logHello);
+target.addEventListener("world", logWorld);
+target.dispatchEvent("hello");
+target.dispatchEvent("world");
